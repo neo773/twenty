6bf4a0e8e79b7127fb53e892ccab3663b9ff3f10
neo773 <neo773@protonmail.com>
2025-08-26 19:45:21 +0530

    wip

---
+++ packages/twenty-front/src/modules/settings/accounts/components/SettingsMessageFoldersEmptyStateCard.tsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+import { t } from '@lingui/core/macro';
+import { IconFolder } from 'twenty-ui/display';
+import { Section } from 'twenty-ui/layout';
+
+const StyledEmptyState = styled.div`
+  align-items: center;
+  color: ${({ theme }) => theme.font.color.tertiary};
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing(2)};
+  padding: ${({ theme }) => theme.spacing(8)};
+  text-align: center;
+`;
+
+export const SettingsMessageFoldersEmptyStateCard = () => {
+  return (
+    <Section>
+      <StyledEmptyState>
+        <IconFolder size="medium" />
+        <div>{t`No folders found for this account`}</div>
+      </StyledEmptyState>
+    </Section>
+  );
+};
